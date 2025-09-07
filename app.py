aede29e6e00574ede478f8c71c146766f2ede5d1
Pedro Schumacher <pprschumacher@gmail.com>
2025-09-07 15:33:40 -0300

    Add Streamlit dashboard and config for car sales
    Introduces a Streamlit dashboard in app.py to visualize car sales data with histogram and scatter plot options. Adds an initial EDA notebook and a Streamlit config file for server settings.

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.header('Dashboard de Anúncios de Venda de Carros')
+
+car_data = pd.read_csv('C:/Users/Mecalor/Documents/GitHub/tpt-sprint-05/vehicles_us.csv') # lendo os dados
+
+hist_button = st.button('Criar histograma de odômetro')
+
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de carros')
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Criar gráfico de dispersão')
+
+if scatter_button:
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de carros')
+
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
+    
+
 
